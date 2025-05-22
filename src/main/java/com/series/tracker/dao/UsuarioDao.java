e81f5ebd94f13f9b05bbdfd58e4bf43a07644d86
Salma Cruz Sánchez <salmasanchezscs99@gmail.com>
2025-05-22 14:38:15 -0600

    22/05/2025 | Se agregan los dao del proyecto

---
+++ src/main/java/com/series/tracker/dao/UsuarioDao.java
@@ -0,0 +1,16 @@
+package com.series.tracker.dao;
+
+import java.util.List;
+import com.series.tracker.models.Usuario;
+
+public interface UsuarioDao {
+    void crearUsuario(Usuario usuario);
+
+    List<Usuario> obtenerUsuarios();
+
+    void modificarUsuario(Usuario usuario);
+
+    void eliminarUsuario(long id);
+
+    Usuario obtenerUsuarioPorCredenciales(Usuario usuario);
+}
